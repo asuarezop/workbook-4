bb145a3b004b933ea846d0bcb360518975fdd162
Anthony Suarez <asuarezop@gmail.com>
2024-10-23 12:08:51 -0400

    Added logic to calculate totalReservationPrice in Reservation class

--- HotelOperations/src/main/java/com/pluralsight/hotel/app/HotelApp.java
+++ HotelOperations/src/main/java/com/pluralsight/hotel/app/HotelApp.java
@@ -1,7 +1,11 @@
 package com.pluralsight.hotel.app;
 
+import com.pluralsight.hotel.models.Reservation;
+
 public class HotelApp {
     public static void main(String[] args) {
+        Reservation r = new Reservation("king", 3, true);
 
+        System.out.printf("Total Price of Reservation: $%.2f", r.getReservationTotal());
     }
 }
--- HotelOperations/src/main/java/com/pluralsight/hotel/models/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/hotel/models/Reservation.java
@@ -4,6 +4,7 @@ public class Reservation {
     private String roomType;
     private int numberOfNights;
     private boolean isWeekend;
+    private double price;
 
     public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
         this.roomType = roomType;
@@ -23,8 +24,26 @@ public class Reservation {
         return isWeekend;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     //Non-static method for Reservation
     public double getReservationTotal() {
-        return 0.0;
+        double totalPrice = 0.0;
+
+        if (roomType.equals("king") && isWeekend) {
+            double priceOnWeekend = (139.00 * 0.10) + 139.00;
+            totalPrice = priceOnWeekend * getNumberOfNights();
+        } else if (roomType.equals("double") && isWeekend) {
+            double priceOnWeekend = (124.00 * 0.10) + 124.00;
+            totalPrice = priceOnWeekend * getNumberOfNights();
+        } else if (roomType.equals("king")) {
+            totalPrice = 139.00 * getNumberOfNights();
+        } else if (roomType.equals("double")) {
+            totalPrice = 124.00 * getNumberOfNights();
+        }
+
+        return totalPrice;
     }
 }
